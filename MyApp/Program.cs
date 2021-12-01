1f68f2efda63737498f75e4cf5088bae67b99249
Amol-salelkar-kilowott <amol.salelkar@kilowott.com>
2021-11-30 19:24:28 -0800

    Add project files.

---
+++ MyApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            int[] a = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+            int n = a.Length;
+
+            Console.Write("Mean = " + Assignment.FindMean(a, n) + "\n");
+            Console.Write("Median = " + Assignment.FindMedian(a, n) + "\n");
+            Console.Write("Median = " + Assignment.Findmode(a, n) + "\n");
+
+        }
+    }
+}
+
+
+       
+
+
